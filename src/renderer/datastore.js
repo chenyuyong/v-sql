1eb300e6af498a2215bd810baa09c7bd5125bf08
chenyuyong <784987545@qq.com>
2023-03-25 19:26:51 +0800

    数据库 实例化

--- src/renderer/datastore.js
+++ src/renderer/datastore.js
@@ -1,19 +1,67 @@
-// import Datastore from 'nedb'
+// eslint-disable-next-line
+import Datastore from 'nedb'
 // import path from 'path'
-// import { remote } from 'electron'
+// eslint-disable-next-line
+const path = require('path')
+// eslint-disable-next-line
+import { remote } from 'electron'
+// const remote = require('@electron/remote')
+// const { remote } = require('electron')
 
 // export default new Datastore({
-//   autoload: true,
-//   filename: path.join(remote.app.getPath('userData'), '/test.db')
+// 	autoload: true,
+// 	filename: path.join(remote.app.getPath('userData'), '/test.db'),
 // })
 
+const db = new Datastore({
+	autoload: true,
+	filename: path.join(remote.app.getPath('userData'), '/test.db'),
+})
 
+console.log(db.find, 88888888888)
 
-// const db = {
-//     test: new Datastore({filename: path.join(remote.app.getPath('userData'), '/test.db')}),
-// }
+// db.find({}, function (err, docs) {
+// 	console.log(docs)
+// 	console.log(err)
+// 	if (!err) {
+// 		console.log(docs)
+// 	}
+// })
+
+// 	console.log(docs)
+// })
+// // 插入数据
+// db.insert({ name: 20, age: 100 }, function (err, doc) {
+// 	if (err) {
+// 		console.log(err)
+// 		return
+// 	}
+// 	console.log(doc)
+// })
+// // 更新数据
+// db.update(
+// 	{ _id: 'cHODtJOIft1YcOMN' },
+// 	{ $set: { name: '赵六' } },
+// 	function (err, data) {
+// 		if (err) {
+// 			console.log(err)
+// 			return
+// 		}
+// 		console.log(data)
+// 	}
+// )
+// // 移除数据
+// db.remove({ id: '6nAYPLImXRs7mB0P' }, {}, function (err, data) {
+// 	if (err) {
+// 		console.log(err)
+// 		return
+// 	}
+// 	console.log(data)
+// })
 // export default {
-//     db
+// 	db,
 // }
 // console.log("datastore--",Datastore)
-console.log("datastore--")
+// var Datastore = require('nedb')
+
+// console.log('datastore--')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+	transpileDependencies: true,
+	configureWebpack: {
+		plugins: [new NodePolyfillPlugin()],
+		externals: {
+			fs: 'require("fs")',
+			path: 'require("path")',
+		},
+	},
+	pluginOptions: {
+		electronBuilder: {
+			nodeIntegration: true,
+		},
+	},
 })
+
+// module.exports = {
+// 	configureWebpack: (config) => {
+// 		const plugins = []
+// 		plugins.push(new NodePolyfillPlugin())
+// 	},
+// }
